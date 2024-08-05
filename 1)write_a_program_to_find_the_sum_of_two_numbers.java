516d217a58f72f94a5d8e4c4972adec20495cc0e
Mahathi-412 <prudhvimahathi725@gmail.com>
2024-08-05 17:56:34 +0530

    1)write_a_program_to_find_the_sum_of_two_numbers.java

---
+++ 1)write_a_program_to_find_the_sum_of_two_numbers.java
@@ -0,0 +1,8 @@
+class AddNumbers{
+    public static void main(String[] args) 
+    {
+        int n1=5,n2=10,sum;
+        sum=n1+n2;
+        System.out.println("The sum of" + n1 + "and" + n2 + "is:"+sum);
+    }
+}
