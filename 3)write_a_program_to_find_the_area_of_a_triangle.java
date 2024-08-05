b4672eb5e8c0199e070a9a54c45070d47d64431b
Mahathi-412 <prudhvimahathi725@gmail.com>
2024-08-05 18:57:56 +0530

    3)write_a_program_to_find_the_area_of_a_triangle.java

--- 3)write_a_program_to_find_the_area_of_a_triangle.java
+++ 3)write_a_program_to_find_the_area_of_a_triangle.java
@@ -1,17 +1,9 @@
 import java.lang.Math;
-
-class AreaofTriangle {
-
+class AreaOfTriangle {
     public static void main(String[] args) {
-
-        int a = 3, b = 9, c = 18;
-        double s, area;
-
-        s = (a + b + c) / 2;
-
-        area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-
-        System.out.println("Area of triangle is: " + area);
-
+        double a = 3, b = 4, c = 5;
+        double s = (a + b + c) / 2.0;
+        double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        System.out.println("Area of the triangle is: " +area);
 }
 }
