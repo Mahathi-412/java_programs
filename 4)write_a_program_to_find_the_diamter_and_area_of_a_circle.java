a09708fe96490e29fac12ecf80c57fe127d0ae9b
Mahathi-412 <prudhvimahathi725@gmail.com>
2024-08-12 15:14:21 +0530

    4)write_a_program_to_find_the_diamter_and_area_of_a_circle.java

---
+++ 4)write_a_program_to_find_the_diamter_and_area_of_a_circle.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        double r=sc.nextDouble();
+        double d=r*2;
+        double a=3.14*r*r;
+        System.out.println(d);
+        System.out.println(a);
+        
+    }
+}
