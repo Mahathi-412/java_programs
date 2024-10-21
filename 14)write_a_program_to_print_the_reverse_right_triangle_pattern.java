6cf4c5941318aa04e7e08f77c3726713f9eb5bb0
Mahathi-412 <prudhvimahathi725@gmail.com>
2024-10-21 15:34:17 +0530

    14)write_a_program_to_print_the_reverse_right_triangle_pattern.java

---
+++ 14)write_a_program_to_print_the_reverse_right_triangle_pattern.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Main
+{
+    public static void main(String[] args) 
+    {
+        for(int i=5;i<=5;i--)
+        {
+          for(int j=1;j<=i;j++)
+          {
+              System.out.println("*");
+          }
+           System.out.println();
+        }
+    }
+}
